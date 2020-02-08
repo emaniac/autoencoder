b71b13e6c07d9e1ffbe3fa82c9b617a9835f7388
emaniac <honza@boucek.cz>
2020-02-08 16:50:28 +0100

    added conv layer

--- src/model.py
+++ src/model.py
@@ -23,18 +23,24 @@ class Encoder(tf.keras.layers.Layer):
             activation=tf.nn.relu,
             kernel_initializer='he_uniform')
 
+        self.conv_layer = tf.keras.layers.Conv1D(filters=32, 
+                                          kernel_size=(3,), 
+                                          activation=tf.nn.sigmoid)
+
         self.output_layer = tf.keras.layers.Dense(
             units=intermediate_dim,
             activation=tf.nn.sigmoid)
         
     def call(self, input_features):
-        activation = self.hidden_layer(input_features)
+        activation = self.conv_layer(input_features)
+        activation = self.hidden_layer(activation)
         return self.output_layer(activation)
 
 class Decoder(tf.keras.layers.Layer):
     
     def __init__(self, intermediate_dim, original_dim):
         super(Decoder, self).__init__()
+
 
         self.hidden_layer = tf.keras.layers.Dense(
             units=intermediate_dim,
