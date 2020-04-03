8e90eb566730b03f87e4e60248165c9d027d7d8b
Jan Boucek <jan.boucek@goodvisionlive.com>
2020-04-03 09:24:09 +0200

    added data download script

---
+++ data/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+tar -xf cifar-10-python.tar.gz
+mv cifar-10-batches-py/* .
+rm -r cifar-10-batches-py cifar-10-python.tar.gz
